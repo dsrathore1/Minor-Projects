b159e6d0749263500a772e90c64a6d263c5c8014
DS Rathore <dsrathorebsw1234@gmail.com>
2021-08-07 15:21:41 +0530

    Clock App

---
+++ Clock_App.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=("ds-digital", 80),
+              background='black', foreground='red', padding=60)
+
+label.pack(anchor="center")
+
+time()
+
+mainloop()
