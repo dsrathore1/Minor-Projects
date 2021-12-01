7d0b8baab6ce9c476cea5555db8fe72336090bc7
DS Rathore <dsrathorebsw1234@gmail.com>
2021-12-01 14:40:29 +0530

    change light on/off button

--- Drive Car/main.js
+++ Drive Car/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
     //lights
     $(document).on('keypress', function (e) {
-        if (e.which == 119) {
+        if (e.which == 108) {
             if (flag) {
                 flag = false;
                 $img.attr('src', cars[0]);
